4c1a4fd2f2fecb11c81e014b0f7fd9d20c125456
Tomasz Tarnawski (PGS Software) <ttarnawski@pgs-soft.com>
2016-09-27 16:22:56 +0200

    Prepare database

---
+++ src/AccessibilityBarriersBundle/Entity/Comment.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AccessibilityBarriersBundle\Entity;
+
+class Comment
+{
+    /** @var integer */
+    private $id;
+
+    /** @var string */
+    private $content;
+
+    /** @var \DateTime */
+    private $createdAt;
+
+    /** @var Issue */
+    private $issue;
+
+    /** @var User */
+    private $user;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return Issue
+     */
+    public function getIssue()
+    {
+        return $this->issue;
+    }
+
+    /**
+     * @param Issue $issue
+     */
+    public function setIssue($issue)
+    {
+        $this->issue = $issue;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+}
