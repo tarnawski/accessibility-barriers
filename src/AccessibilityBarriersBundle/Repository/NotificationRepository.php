b5d0f27777ea9b06c0cb53961fd1ac2fd95b8370
Tomasz <tarnawski27@gmail.com>
2016-10-03 21:32:17 +0200

    Add query endpoint

--- src/AccessibilityBarriersBundle/Repository/NotificationRepository.php
+++ src/AccessibilityBarriersBundle/Repository/NotificationRepository.php
@@ -5,10 +5,22 @@ namespace AccessibilityBarriersBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
 /**
- * Class CategoryReNotificationRepositorypository
+ * Class NotificationRepository
  */
 class NotificationRepository extends EntityRepository
 {
+    public function findByQuery($query = null)
+    {
+        $builder = $this->createQueryBuilder('n');
+        if ($query) {
+            $builder->andWhere('(n.name LIKE :query) OR (n.description LIKE :query)');
+            $builder->setParameter('query', '%' . $query. '%');
+        }
+        $builder->addOrderBy('n.createdAt', 'DESC');
+
+        return $builder->getQuery()->getResult();
+    }
+
     public function getNotificationsCount()
     {
         $result = $this->createQueryBuilder('c')
